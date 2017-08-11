6ffd1cd5173138a77f0ed74fed6118c429efda32
Heiko Lang <h.lang@sprinteins.com>
2017-08-11 10:03:51 +0200

    datenbank file aktualisiert

--- production/datenbank.sql
+++ production/datenbank.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Erstellungszeit: 09. Aug 2017 um 18:19
+-- Erstellungszeit: 11. Aug 2017 um 10:01
 -- Server-Version: 10.1.25-MariaDB
 -- PHP-Version: 5.6.31
 
@@ -21,6 +21,75 @@ SET time_zone = "+00:00";
 --
 -- Datenbank: `databases`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Tabellenstruktur für Tabelle `avatars`
+--
+
+CREATE TABLE `avatars` (
+  `avatarid` int(255) NOT NULL,
+  `imgdata` longblob NOT NULL,
+  `imgname` varchar(255) NOT NULL,
+  `imgsize` int(11) NOT NULL,
+  `imgtype` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Tabellenstruktur für Tabelle `parties`
+--
+
+CREATE TABLE `parties` (
+  `SpielID` int(255) NOT NULL,
+  `Spieler1` varchar(255) NOT NULL DEFAULT 'noone',
+  `Spieler2` varchar(255) NOT NULL DEFAULT 'noone',
+  `AmZug` varchar(255) NOT NULL DEFAULT '0',
+  `1` varchar(255) NOT NULL DEFAULT '0',
+  `2` varchar(255) NOT NULL DEFAULT '0',
+  `3` varchar(255) NOT NULL DEFAULT '0',
+  `4` varchar(255) NOT NULL DEFAULT '0',
+  `5` varchar(255) NOT NULL DEFAULT '0',
+  `6` varchar(255) NOT NULL DEFAULT '0',
+  `7` varchar(255) NOT NULL DEFAULT '0',
+  `8` varchar(255) NOT NULL DEFAULT '0',
+  `9` varchar(255) NOT NULL DEFAULT '0',
+  `10` varchar(255) NOT NULL DEFAULT '0',
+  `11` varchar(255) NOT NULL DEFAULT '0',
+  `12` varchar(255) NOT NULL DEFAULT '0',
+  `13` varchar(255) NOT NULL DEFAULT '0',
+  `14` varchar(255) NOT NULL DEFAULT '0',
+  `15` varchar(255) NOT NULL DEFAULT '0',
+  `16` varchar(255) NOT NULL DEFAULT '0',
+  `17` varchar(255) NOT NULL DEFAULT '0',
+  `18` varchar(255) NOT NULL DEFAULT '0',
+  `19` varchar(255) NOT NULL DEFAULT '0',
+  `20` varchar(255) NOT NULL DEFAULT '0',
+  `21` varchar(255) NOT NULL DEFAULT '0',
+  `22` varchar(255) NOT NULL DEFAULT '0',
+  `23` varchar(255) NOT NULL DEFAULT '0',
+  `24` varchar(255) NOT NULL DEFAULT '0',
+  `25` varchar(255) NOT NULL DEFAULT '0',
+  `26` varchar(255) NOT NULL DEFAULT '0',
+  `27` varchar(255) NOT NULL DEFAULT '0',
+  `28` varchar(255) NOT NULL DEFAULT '0',
+  `29` varchar(255) NOT NULL DEFAULT '0',
+  `30` varchar(255) NOT NULL DEFAULT '0',
+  `31` varchar(255) NOT NULL DEFAULT '0',
+  `32` varchar(255) NOT NULL DEFAULT '0',
+  `33` varchar(255) NOT NULL DEFAULT '0',
+  `34` varchar(255) NOT NULL DEFAULT '0',
+  `35` varchar(255) NOT NULL DEFAULT '0',
+  `36` varchar(255) NOT NULL DEFAULT '0',
+  `37` varchar(255) NOT NULL DEFAULT '0',
+  `38` varchar(255) NOT NULL DEFAULT '0',
+  `39` varchar(255) NOT NULL DEFAULT '0',
+  `40` varchar(255) NOT NULL DEFAULT '0',
+  `41` varchar(255) NOT NULL DEFAULT '0',
+  `42` varchar(255) NOT NULL DEFAULT '0'
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -46,11 +115,23 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id`, `nickname`, `avatarid`, `username`, `mail`, `password`, `useralter`, `geschlecht`, `challenge`, `confirmed`) VALUES
-(1, 'user1', 0, 'dhbw1', 'user1@test.de', '$2y$10$Os/dW0Q8XUDr742NE39EyOfe2bhQ9YoIhcdoACvSsBG8k0DaATiB.', 20, 'female', '2b4267f96a2b4af113f1b549df549aca', 1);
+(1, 'user1', 0, 'dhbw1', 'user1@test.de', '$2y$10$Os/dW0Q8XUDr742NE39EyOfe2bhQ9YoIhcdoACvSsBG8k0DaATiB.', 20, 'male', '2b4267f96a2b4af113f1b549df549aca', 1);
 
 --
 -- Indizes der exportierten Tabellen
 --
+
+--
+-- Indizes für die Tabelle `avatars`
+--
+ALTER TABLE `avatars`
+  ADD PRIMARY KEY (`avatarid`);
+
+--
+-- Indizes für die Tabelle `parties`
+--
+ALTER TABLE `parties`
+  ADD PRIMARY KEY (`SpielID`);
 
 --
 -- Indizes für die Tabelle `users`
@@ -64,10 +145,20 @@ ALTER TABLE `users`
 --
 
 --
+-- AUTO_INCREMENT für Tabelle `avatars`
+--
+ALTER TABLE `avatars`
+  MODIFY `avatarid` int(255) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT für Tabelle `parties`
+--
+ALTER TABLE `parties`
+  MODIFY `SpielID` int(255) NOT NULL AUTO_INCREMENT;
+--
 -- AUTO_INCREMENT für Tabelle `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;COMMIT;
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
